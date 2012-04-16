de8ff236708529a529deace41260532e4d9edbe8
David Ellis <d.f.ellis@ieee.org>
2012-04-16 10:08:14 -0700

    Some source-code cleanup, and fixing the 'Hello World' code so it runs the 'Goodbye World' path.

--- deployment.js
+++ deployment.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 // # The *Deployment* Server
 
 // Load vendor modules
@@ -9,52 +9,33 @@ var fs = require('fs');
 // Load the configuration file
 var config = JSON.parse(fs.readFileSync(path.join(__dirname + "/config.json"), "utf8"));
 
-var server = express();
+var server = express.createServer();
 
 //Setting up server configuration
 server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.set('view engine', 'html');
-    server.use(express.favicon());
-    server.use(express.logger('dev'));
-    server.use(express.static(__dirname + '/public'));
-    server.use(express.bodyParser());
-    server.use(express.methodOverride());
-    server.use(server.router);
+	server.set('views', __dirname + '/views');
+	server.set('view engine', 'html');
+	server.use(express.favicon());
+	server.use(express.logger('dev'));
+	server.use(express.static(__dirname + '/public'));
+	server.use(express.methodOverride());
+	server.use(server.router);
 
 });
-
-//Setting up error handling
-/*server.error(function(err, req, res, next){
-    if (err instanceof SyntaxError) {
-        res.render('404.jade');
-    } else {
-        next(err);
-    }
-});
-*/
 
 //Rendering index.html on '/' url get
 server.get('/', function(req, res){
-    res.render('index.html');
+	res.render('index.html');
 
 });
 
 //Setting up action on '/sayhello' url post
-server.post('/sayhello', function(req, res){
- 
-    //For some reason, catch does not work when 
-    //stringify throws an exception
-    try{
-	var result = JSON.stringify(req.body);
+server.post('/sayhello', express.bodyParser(), function(req, res) {
 	res.send("Hello World!!! :D");
-	console.log("\nSaying hello world " + JSON.stringify(req.body));
-    }catch(error){
+}, function(err, req, res, next) {
 	res.send("Good Bye World :(");
-	console.log("\nSaying good bye world " + JSON.stringify(req.body)  + "   "+error);
-    }
 });
-    
+
 //Starting listener on configured port and host
-server.listen(config.rpc.port,config.rpc.host)
+server.listen(config.rpc.port,config.rpc.host);
 console.log('Deployment server running on port "' + config.rpc.port);
