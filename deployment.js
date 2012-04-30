5b22f7e02b345272df49b66ca426401c98a78bca
Francelice Sarda <francelice@gmail.com>
2012-04-30 00:41:29 -0430

    Rebuild of config files and checking authorization start

--- deployment.js
+++ deployment.js
@@ -8,6 +8,9 @@ var fs = require('fs');
 
 // Load the configuration file
 var config = JSON.parse(fs.readFileSync(path.join(__dirname + "/config.json"), "utf8"));
+var modules = JSON.parse(fs.readFileSync(path.join(__dirname + "/deployment-modules.json"), "utf8"));
+var servers = JSON.parse(fs.readFileSync(path.join(__dirname + "/deployment-servers.json"), "utf8"));
+var configDeployData = JSON.parse(fs.readFileSync(path.join(__dirname + "/deployment-config.json"), "utf8"));
 
 var server = express.createServer();
 
@@ -20,25 +23,76 @@ server.configure(function(){
     server.use(express.static(__dirname + '/public'));
     server.use(express.methodOverride());
     server.use(server.router);
-
+    
 });
 
-//Rendering index.html on '/' url get
-server.get('/', function(req, res){
-    res.sendfile(__dirname + '/views/index.html');
-});
+//Verifies if the request comes from  github
+var isRequestFromGitHubRepository = function(request, configData){
+    return request.repository!=undefined && configData.repositoryName.toUpperCase() === request.repository.name.toUpperCase();
+}
 
-//Setting up action on '/sayhello' url post
-server.post('/sayhello', express.bodyParser(), function(req, res) {
 
-    if(req.body.ref.indexOf("production")!= -1){
-	res.send("Proccessing...");
+//Verifies conditions for running authomatized deployment
+var isAuthorizedProductionChange = function(request, configData){
+    
+    //Check if is a production branch push
+    var response = request.ref.indexOf(configData.productionBranch)!=-1;
+    
+    //Not everyone is authorized. Let's check if the pusher is
+    if(configData.authorizedCommitUsers.length!=0){
+	response = response && configData.authorizedCommitUsers.indexOf(request.pusher.name) != -1;
+    }
+
+    return response;
+
+}
+
+//Get list of servers where modifications where made
+var getChangesLocation = function(request){
+    
+    var modules = [];
+
+    //collect the path changed
+    for(commit in request.commits){
+	
+    }
+}
+
+//Get dependency subGraph
+var getDependencySubGraph = function(request,serversModified){
+
+}
+
+
+//Setting up action on '/deployment' url post
+server.post('/deployment', express.bodyParser(), function(req, res) {
+
+    if(isRequestFromGitHubRepository(req.body, configDeployData)){
+	console.log("This push is from github's " + configDeployData.repositoryName + " repository");
+
+	if(isAuthorizedProductionChange(req.body, configDeployData)){	    
+	    console.log("This is an authorized production branch push");
+	    
+	    servers = getChangesLocation(req);
+	    graph = getDependencySubGraph(servers);
+	    
+	    try{
+		//execute scripts
+	    }catch(err){
+		
+	    }
+	    
+	    res.send("Proccessing...");
+	}else{
+	    res.send("This request does not correspond to an authorized production change " + req);	    
+	}
+	
     }else{
-	res.send("Hello World!!! :D");
+	res.send("This request does not have git hub hook's format " + req);
     }
     
 }, function(err, req, res, next) {
-    res.send("Good Bye World :(");
+    res.send("An error has occurred processing the request..." + err);
 });
 
 //Starting listener on configured port and host
