b6f11aba08f1974e046cdfcf26144cb50c64523b
fsarda <francelice.agrosica@agrosica.com>
2012-07-17 01:05:20 -0430

    Executing local install without retrying. Modifying init scripts to redirect output to log files

--- srv/deploymentChild.js
+++ srv/deploymentChild.js
@@ -1,12 +1,24 @@
 var childl = require('child_process');
 var date = new Date();
+var commandReceived = {};
+var retryCount = 0;
+
 
 process.on('message', function(m) {
     console.log("\n["+date+"] Executing child process with command " + m.command)
+    commandReceived = m;
     childl.exec(m.command , m.options, sendToParent);
 });
 
 
 function sendToParent(error, stdout, stderr){
-    process.send({ "code":error , "stdout": stdout, "stderr": stderr });    
+
+    console.log("\n["+date+"] Processing ending exec with response {" +error + ", "+stdout+ ", "+stderr+"}");
+
+    // if(retryCount < commandReceived.retryCount && error != null){
+    // 	retryCount++;
+    // 	console.log("\n["+date+"] Retrying  " + commandReceived.command+" with attempt "+retryCount);
+    // }else{
+	process.send({ "code":error , "stdout": stdout, "stderr": stderr });    
+    //}
 }
