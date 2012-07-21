ce50e241039752afffa32b8a2843106a6d8e79de
fsarda <francelice.agrosica@agrosica.com>
2012-07-20 22:21:01 -0430

    Fixing issue with restart dependencies. Commenting retry to test without it

--- srv/deploymentChild.js
+++ srv/deploymentChild.js
@@ -3,7 +3,7 @@ var date = new Date();
 var commandReceived = {};
 var retryCount = 0;
 
-
+//Process receives a message from parent 
 process.on('message', function(m) {
     console.log("\n["+date+"] Executing child process with command " + m.command)
     commandReceived = m;
@@ -11,14 +11,20 @@ process.on('message', function(m) {
 });
 
 
+//Sends message to parent process with result of execution
 function sendToParent(error, stdout, stderr){
 
-    console.log("\n["+date+"] Processing ending exec with response {" +error + ", "+stdout+ ", "+stderr+"}");
+    console.log("\n["+date+"] Processing ending exec with response {" +error + ", "+commandReceived.command+"}");
 
-    // if(retryCount < commandReceived.retryCount && error != null){
+    // if(retryCount < commandReceived.retryCount && (error != null || (stderr != "" && stderr != null))){
     // 	retryCount++;
     // 	console.log("\n["+date+"] Retrying  " + commandReceived.command+" with attempt "+retryCount);
+    // 	childl.exec(commandReceived.command , commandReceived.options, sendToParent);
     // }else{
-	process.send({ "code":error , "stdout": stdout, "stderr": stderr });    
-    //}
+    // 	if(stderr != "" && stderr != null){
+    // 	    process.send({ "code":stderr , "stdout": stdout, "stderr": stderr });    
+    // 	}else{
+    process.send({ "code":error , "stdout": stdout, "stderr": stderr });    
+    // 	}
+    // }
 }
